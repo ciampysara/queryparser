4f3473dfa362b1938c81a11cdd90cbf87340c13d
Cristian Lorenzetto <cristian.lorenzetto@comune.fi.it>
2020-07-17 08:35:19 +0200

    update

--- tests/main_test.go
+++ tests/main_test.go
@@ -3,16 +3,19 @@ package tests
 import (
 	"fmt"
 	"github.com/publicocean0/queryparser"
+	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
-
-	q, exception := queryparser.Parse("  aa       3    ")
+	src := "  aa       3    "
+	q, exception := queryparser.Parse(src)
 	if exception != nil {
-		fmt.Println(exception.Error() + "\r" + exception.Cursor())
+		fmt.Println(exception.Error() + ":\r" + src + "\r" + exception.Cursor())
 		return
+	} else {
+		fmt.Println(*q)
 	}
-	fmt.Println(*q)
+	os.Exit(m.Run())
 
 }
